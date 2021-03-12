b85fb223a7ecdb4561c827afb964ee6212e215a4
LatyshevIV <80543803+LatyshevIV@users.noreply.github.com>
2021-03-12 22:11:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ $(function(){
 	$('.feedbacks__slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow: ' <button class="slick-btn slick-prev"><img src="images/arrow_left.svg" alt="prev"></button>',
-  nextArrow: ' <button class="slick-btn slick-next"><img src="images/arrow_right.svg" alt="next"></button>',
+  prevArrow: ' <button class="slick-btn slick-prev"><img src="arrow_left.svg" alt="prev"></button>',
+  nextArrow: ' <button class="slick-btn slick-next"><img src="arrow_right.svg" alt="next"></button>',
 
 });
 
