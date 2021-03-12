ae720acd8d2b9c3c3344508cd0d0b1183497d922
LatyshevIV <80543803+LatyshevIV@users.noreply.github.com>
2021-03-12 22:05:14 +0300

    Add files via upload
    Загрузка сайта

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	$('.feedbacks__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: ' <button class="slick-btn slick-prev"><img src="images/arrow_left.svg" alt="prev"></button>',
+  nextArrow: ' <button class="slick-btn slick-next"><img src="images/arrow_right.svg" alt="next"></button>',
+
+});
+
+
+});
